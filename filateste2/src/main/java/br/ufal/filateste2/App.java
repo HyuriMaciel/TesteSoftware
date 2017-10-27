7821a042ccb01bb14e1cfae6c2cd2f377db62d7b
Hyuir Maciel <smhyuri@gmail.com>
2017-10-27 09:18:36 -0300

    mudança main

--- filateste2/src/main/java/br/ufal/filateste2/App.java
+++ filateste2/src/main/java/br/ufal/filateste2/App.java
@@ -8,12 +8,6 @@ public class App
 	 public static void main( String[] args )
 	    {
 	    	HRQueue<Integer> q = new HRQueue<Integer>(5);
-	        for (int i = 0; i < 10; i++) {
-	            System.out.println(i);
-	            q.push(i);
-	        }
-	        for (int i = 0; q.empty()!= true; i++) {
-	            System.out.println(q.pop());
-	        }
+	       
 	    }
 }
